f5591880ed9edc07450a66fba8d446aedf702fe3
Resist <93611543+Resisst@users.noreply.github.com>
2022-06-26 16:00:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 import os
 
-requirements = """httpx\npyotp\npsutil\npypiwin32\npycryptodome\npyinstaller>=4.9\nPIL-tools"""
-for requirement in requirements.split("\n"):
-    requirement = requirement.split(">")
-    print("Installing {}".format(requirement[0]))
-    os.system(f"py -m pip install {'>'.join(requirement)} >nul")
+print("Installing requirements")
+os.system(f"py -m pip install -r requirements.txt >nul")
     
 print("Installed all requirements!")
 print("Starting build-exe.bat...")
